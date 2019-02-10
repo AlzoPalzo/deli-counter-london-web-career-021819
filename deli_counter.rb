a36b9229fdc823e230f9b6a71b5830c0daa296d6
Ali Madni <ali555m@hotmail.com>
2019-02-10 16:16:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(line, name)
   line.push(name)
-  puts("Welcome, #{name}. You are number #{line.last + 1} in line.")
+  puts("Welcome, #{name}. You are number #{line.length + 1} in line.")
 end
 
 def now_serving
