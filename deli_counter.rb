d7919b12d60e893608b5693fd8aced64261c9078
Ali Madni <ali555m@hotmail.com>
2019-02-10 16:03:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(line)
+  if line.length == 0
+    puts("The line is currently empty.")
+  else
+    "The line is currently: "
+  end
+end
+
+def take_a_number(line, name)
+
+end
+
+def now_serving
+
+end
